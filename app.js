031d0667465b730f31c98ab0848f00685502d948
nathanclem100 <nathanclem100@gmail.com>
2023-12-03 13:52:59 -0600

    Initial Commit *v1.0*

---
+++ app.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const http = require('http');
+const socketIO = require('socket.io');
+
+const app = express();
+const server = http.createServer(app);
+const io = socketIO(server);
+
+app.use(express.static('public'));
+
+io.on('connection', (socket) => {
+    console.log('A user connected');
+
+    socket.on('chat message', (msg) => {
+        io.emit('chat message', msg);
+    });
+
+    socket.on('disconnect', () => {
+        console.log('User disconnected');
+    });
+});
+
+server.listen(3000, () => {
+    console.log('listening on *:3000');
+});
